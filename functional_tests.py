dba776fcff5c70e8efd8e515f72cd39c28601d76
Yan P Aung <yan.pye.aung@gmail.com>
2014-10-22 07:23:46 -0700

    Add test to save second item

--- functional_tests.py
+++ functional_tests.py
@@ -35,17 +35,23 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
-        
+
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-                any(row.text == '1: Buy peacock feathers' for row in rows),
-                "New to-do item did not appear in table"
-        )
-
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+            
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very 
         # methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('2: Use peacock feathers to make a fly',
+            [row.text for row in rows])
+
         self.fail('Finish the test!')
 
         # The page updates again, and now shows both items on her list
