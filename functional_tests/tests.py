6e0383f3e4779dc013566a309f97e285504fc8ec
Yan P Aung <yan.pye.aung@gmail.com>
2015-02-01 14:17:36 -0800

    Typo

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -66,7 +66,7 @@ class NewVisitorTest(LiveServerTestCase):
         # list
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('Buy peacock feathers', page_next)
+        self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
 
         # Francis starts a new list by entering a new item. He
