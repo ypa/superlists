0980ac52f6d15969473d052099f4077bd3d4ecb4
Yan Aung <yan.pye.aung@gmail.com>
2015-10-09 21:15:48 -0700

    Add unique constraint to models

--- lists/models.py
+++ lists/models.py
@@ -10,3 +10,6 @@ class Item(models.Model):
     text = models.TextField(default= '')
     list = models.ForeignKey(List, default= None)
 
+    class Meta:
+        unique_together = ('list', 'text')
+
