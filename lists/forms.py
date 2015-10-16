5740e241ec57dbe2406007788b759cf9e69b288a
Yan P Aung <yan.pye.aung@gmail.com>
2015-10-15 20:48:20 -0700

    Add Duplicate Item Validation form

--- lists/forms.py
+++ lists/forms.py
@@ -1,9 +1,10 @@
 
 from django import forms
+from django.core.exceptions import ValidationError
 from lists.models import Item
 
 EMPTY_LIST_ERROR = "You can't have an empty list item"
-
+DUPLICATE_ITEM_ERROR = "You've already got this item in your list"
 
 class ItemForm(forms.models.ModelForm):
 
@@ -23,4 +24,18 @@ class ItemForm(forms.models.ModelForm):
         error_messages = {
             'text':{'required':EMPTY_LIST_ERROR}
         }
+
+
+class ExistingListItemForm(ItemForm):
+
+    def __init__(self, for_list, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.instance.list = for_list
+
+    def validate_unique(self):
+        try:
+            self.instance.validate_unique()
+        except ValidationError as e:
+            e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
+            self._update_errors(e)
 
