f1ace2a64f4dae32c80f34ae30f081dc32fd1ea7
Yan P Aung <yan.pye.aung@gmail.com>
2016-04-07 20:44:12 -0700

    Update views with ExistingListItemForm and update tests
    Minor test fix

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -43,5 +43,5 @@ class ItemValidationTest(FunctionalTest):
         # She sees a helpful error message
         self.check_for_row_in_list_table('1: Buy wellies')
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "You've already got this in your list")
+        self.assertEqual(error.text, "You've already got this item in your list")
 
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -6,7 +6,10 @@ from django.utils.html import escape
 
 from lists.views import home_page
 from lists.models import Item, List
-from lists.forms import ItemForm, EMPTY_LIST_ERROR
+from lists.forms import (
+    DUPLICATE_ITEM_ERROR, EMPTY_LIST_ERROR,
+    ExistingListItemForm, ItemForm
+)
 
 from unittest import skip
 
@@ -93,7 +96,7 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_displays_error_on_page(self):
         response = self.post_invalid_input()
@@ -102,10 +105,9 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get('/lists/%d/' % (list_.id,))
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
@@ -113,7 +115,7 @@ class ListViewTest(TestCase):
             '/lists/%d/' % (list1.id,),
             data={'text': 'textey'}
         )
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
