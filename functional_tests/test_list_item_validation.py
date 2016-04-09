98a5085347237d73d999fbfb170bbeed8fb28867
Yan P Aung <yan.pye.aung@gmail.com>
2016-04-09 11:30:44 -0700

    Add FT for error message disappears when typing in inputbox

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -3,6 +3,9 @@ from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_list_items(self):
         # Edith goes to the home page and accidentally tries to submit
         # an empty list item. She hits Emter on the empty input box
@@ -23,7 +26,7 @@ class ItemValidationTest(FunctionalTest):
 
         # She receives a similar warning on the list page
         self.check_for_row_in_list_table('1: Buy milk')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # And she can correct it by filling some text in
@@ -42,6 +45,19 @@ class ItemValidationTest(FunctionalTest):
 
         # She sees a helpful error message
         self.check_for_row_in_list_table('1: Buy wellies')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You've already got this item in your list")
 
+    def test_error_messages_are_cleared_on_input(self):
+        # Edith starts a new list in a way that causes a validation error
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('\n')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertTrue(error.is_displayed())
+
+        # she starts typing in the input box to clear the error
+        self.get_item_input_box().send_keys('abc')
+
+        # she is pleased to see that the error message disappeared.
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
