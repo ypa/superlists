4a8d3ab5a76d712b315cf6dbf2842cb419db5dab
Yan P Aung <yan.pye.aung@gmail.com>
2016-05-02 20:16:40 -0700

    Add logout

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -24,9 +24,20 @@ class LoginTest(FunctionalTest):
         self.switch_to_new_window('To-Do')
 
         # She can see that she is logged in
-        self.wait_for_element_with_id('id_logout')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn('edith@mockmyid.com', navbar.text)
+        self.wait_to_be_logged_in()
+
+        # Refreshing the page, she sees it's a real session login,
+        # Not just a one-off for that page.
+        self.browser.refresh()
+        self.wait_to_be_logged_in()
+
+        # Terrified of this new feature, she reflexively clicks "logout"
+        self.browser.find_element_by_id('id_logout').click()
+        self.wait_to_be_logged_out()
+
+        # The 'logged out' status also persists after a refresh
+        self.browser.refresh()
+        self.wait_to_be_logged_out()
 
     def switch_to_new_window(self, text_in_title):
         retries = 60
@@ -41,6 +52,18 @@ class LoginTest(FunctionalTest):
 
     def  wait_for_element_with_id(self, element_id):
         WebDriverWait(self.browser, timeout=30).until(
-            lambda b: b.find_element_by_id(element_id)
+            lambda b: b.find_element_by_id(element_id),
+            'Could not find element with id {}. Page text was {}'.format(
+                element_id, self.browser.find_element_by_tag_name('body').text
+            )
         )
 
+    def wait_to_be_logged_in(self):
+        self.wait_for_element_with_id('id_logout')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn('edith@mockmyid.com', navbar.text)
+
+    def wait_to_be_logged_out(self):
+        self.wait_for_element_with_id('id_login')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn('edith@mockmyid.com', navbar.text)
