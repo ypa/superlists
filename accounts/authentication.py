88ed78d207ea415e95f80735a58a0a40fc280080
Yan P Aung <yan.pye.aung@gmail.com>
2016-05-14 07:44:31 -0700

    Fix/Move DOMAIN to settings to fix Persona login bug

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -1,21 +1,17 @@
-import logging
 import requests
+from django.conf import settings
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
 PERSONA_VERIFY_URL = 'https://verifier.login.persona.org/verify'
-DOMAIN = 'localhost'
 
 class PersonaAuthenticationBackend(object):
 
     def authenticate(self, assertion):
-        logging.warning('entering authenticate function')
         response = requests.post(
             PERSONA_VERIFY_URL,
-            data={'assertion': assertion, 'audience': DOMAIN}
+            data={'assertion': assertion, 'audience': settings.DOMAIN}
         )
-        logging.warning('got response from persona')
-        logging.warning(response.content.decode())
         if response.ok and response.json()['status'] == 'okay':
             email = response.json()['email']
             try:
--- superlists/settings.py
+++ superlists/settings.py
@@ -24,7 +24,9 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+DOMAIN = "localhost"
+
+ALLOWED_HOSTS = [DOMAIN]
 
 
 # Application definition
