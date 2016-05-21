ed9cc6c9751b7106b57759b89e3292def9715e98
Yan P Aung <yan.pye.aung@gmail.com>
2016-05-20 21:02:59 -0700

    url, placeholder view and first-cut templates for my_lists

--- lists/urls.py
+++ lists/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
-    url(r'^new$', 'lists.views.new_list', name= 'new_list'),
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^new$', 'lists.views.new_list', name='new_list'),
+    url(r'^users/(.+)/$', 'lists.views.my_lists', name='my_lists'),
 )
